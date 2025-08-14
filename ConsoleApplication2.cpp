94125a0834249d11e201815aa4b5552ca114bfe0
MalenaDavies <malenadavies5@gmail.com>
2025-08-14 09:49:00 -0300

    op2

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -4,27 +4,14 @@ using namespace std;
 
 int main()
 {
-    char opciones;
-    double gramos = 0, kilogramos = 0, metros = 0, centimetros = 0;
-    cout << "Bienvenido al conversor de unidades. Que desea convertir?" << endl;
-    cout << "Gramos a kilogramos (G). Metros a centimetros (M).";
-    cin >> opciones;
+	int N;
+	double  suma=0;
+	cout << "Ingrese la cantidad de numeros que quiere sumar.";
+	cin >> N;
 
-    if (opciones == 'G') {
-        cout << "Ingrese los gramos para pasar a kilogramos: ";
-        cin >> gramos;
-        kilogramos = gramos / 1000;
-        cout << "Los " << gramos << " gramos en kilogramos son: " << kilogramos;
-    }
-
-    else if (opciones == 'M') {
-        cout << "Ingrese los metros para pasar a centimetros: ";
-        cin >> metros;
-        centimetros = metros * 1000;
-        cout << "Los " << metros << " gramos en centimetros son: " << centimetros;
-    }
-
-    else {
-        cout << "Por favor, ingrese GK o MC.";
-    }
+	for (int i = 1; i <= N; i++) {
+		suma += i * i;
+	}
+	cout<<"la ssuma de los cuadrados de 1 a " << N << " es: " << suma;
+	return 0;
 }
